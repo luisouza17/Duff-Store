0c25d009ce21f719f489edcea505e43e3821f786
LUIS CARLOS BATISTA DE SOUZA <396301368@alunos.etec.itaqua>
2019-04-18 22:31:39 -0300

    Realizado a insercao das cervejas adicionadas na pagina addcerveja para a home

--- src/app/add-cerveja/add-cerveja.page.ts
+++ src/app/add-cerveja/add-cerveja.page.ts
@@ -17,8 +17,20 @@ export class AddCervejaPage implements OnInit {
     const cervejaString = JSON.stringify(form.value)
     const nomeCerveja = form.value.nome
 
-    sessionStorage.setItem(nomeCerveja,cervejaString)
+    sessionStorage.setItem(nomeCerveja,cervejaString);
+    
+    let chavesdisponiveis = sessionStorage.getItem('chaves');
+    
+    if(chavesdisponiveis != null){
+      chavesdisponiveis = chavesdisponiveis + ';' + nomeCerveja;
+    }else{
+      chavesdisponiveis = nomeCerveja;
+    }
+    
+    sessionStorage.setItem('chaves',chavesdisponiveis);
+
     form.reset();
     this.mensagem="Cadastrado com sucesso";
+    console.log("As chaves disponíveis são:",chavesdisponiveis);
   }
 }
--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,10 +1,29 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ViewCompiler } from '@angular/compiler';
 
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
   styleUrls: ['home.page.scss'],
 })
-export class HomePage {
+export class HomePage implements OnInit {  
+  cervejas;
 
+  ngOnInit(){
+  }  
+  ionViewDidEnter(){
+    this.cervejas = [];
+
+    const chavesdisponiveis = sessionStorage.getItem('chaves');
+    
+    const chavesseparadas = chavesdisponiveis.split(';');
+    
+    for(var i = 0 ; i<chavesseparadas.length; i++){
+      const cerveja = sessionStorage.getItem(chavesseparadas[i]);
+      const cervejaObj = JSON.parse(cerveja);
+      this.cervejas.push(cervejaObj); 
+    
+    }
+    }
+  
 }
